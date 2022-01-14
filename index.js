862059021a7a9497dfe86f4f481bb710e7a22816
nadgirs <48817859+nadgirs@users.noreply.github.com>
2022-01-15 00:51:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) =>{
-    res.send('Hello Santosh');
+    res.send('Hello Vihan');
 });
 
 app.listen(4000);
