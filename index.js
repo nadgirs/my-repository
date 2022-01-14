f0e06fd91670b6e22b3b34fdacb37506f9846379
nadgirs <nadgir.santosh@gmail.com>
2022-01-15 01:18:29 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) =>{
-    res.send('Hello Vihan');
+    res.send('Hello Ganesha');
 });
 
 app.listen(4000);
