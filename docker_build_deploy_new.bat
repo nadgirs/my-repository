9eb56726e8aa05b7b8828f7ab16a109296a038f9
nadgirs <nadgir.santosh@gmail.com>
2022-01-15 15:43:38 +0530

    Modified batch file

--- docker_build_deploy_new.bat
+++ docker_build_deploy_new.bat
@@ -2,3 +2,15 @@ cd C:\NodeJSProject\my-repository
 FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker stop %%i && docker rm %%i
 docker build -t my-app:1.4
 docker run -d -p9000:4000 my-app:1.4
+
+REM docker tag my-app:1.2 my-image-repository/my-app:1.2
+REM docker login -u "santosh2507" -p "mylove1!" docker.io/library/my-image-repository
+REM docker push santosh2507/my-image-repository/my-app:1.2
+
+
+REM docker build -t my-app .
+REM docker tag my-app santosh2507/my-image-repository:my-app
+REM docker login
+	santosh2507
+	mylove1!
+REM docker push  santosh2507/my-image-repository:my-app
