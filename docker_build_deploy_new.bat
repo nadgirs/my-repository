4947b23b5c51bca417e1f6862484984a5f232851
nadgirs <nadgir.santosh@gmail.com>
2022-01-15 15:55:48 +0530

    Modified batch file

--- docker_build_deploy_new.bat
+++ docker_build_deploy_new.bat
@@ -3,6 +3,7 @@ FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker stop %%i && docker rm %%i
 docker build -t my-app:1.4
 docker run -d -p9000:4000 my-app:1.4
 
+
 REM docker tag my-app:1.2 my-image-repository/my-app:1.2
 REM docker login -u "santosh2507" -p "mylove1!" docker.io/library/my-image-repository
 REM docker push santosh2507/my-image-repository/my-app:1.2
