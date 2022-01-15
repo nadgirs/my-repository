087e7bb05458975449bc5595ed87ccfb8af7022d
nadgirs <nadgir.santosh@gmail.com>
2022-01-15 16:00:01 +0530

    Modified batch file

--- docker_build_deploy_new.bat
+++ docker_build_deploy_new.bat
@@ -1,6 +1,6 @@
 cd C:\NodeJSProject\my-repository
 FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker stop %%i && docker rm %%i
-docker build -t my-app:1.4
+docker build -t my-app:1.4 .
 docker run -d -p9000:4000 my-app:1.4
 
 
